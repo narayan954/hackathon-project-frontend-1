2664613a52a321461c611e8cc82e362060cc2eb0
Om Budhiraja <om@trisetra.com>
2022-03-26 09:39:44 +0530

    changed server url

--- utils/http.js
+++ utils/http.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const apiCall = (token = '') => {
   const http = axios.create({
-    // baseURL: 'https://boiling-depths-77572.herokuapp.com/api',
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://boiling-depths-77572.herokuapp.com/api',
+    // baseURL: 'http://localhost:5000/api',
     headers: {
       authentication: token ? `Bearer ${token}` : '',
     },
